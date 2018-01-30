c967dc5dc45bc901ec01923d13a56eaf843822d4
Anthony Chu <antchu@microsoft.com>
2018-01-29 22:18:15 -0800

    Add upload to blob storage

--- nuxt-www/app/Api.js
+++ nuxt-www/app/Api.js
@@ -26,8 +26,45 @@ class Api {
 
   uploadImage(file) {
     console.dir(file)
-    return new Promise(resolve => setTimeout(resolve, 4000))
+    return this._getUploadSasUri(file.name)
+      .then(sasUrl => {
+        return this._uploadBlob(sasUrl, file)
+      })
+  }
+
+  _getUploadSasUri(filename) {
+    const config = {
+      headers: {
+        'X-ZUMO-AUTH': this.authToken
+      },
+      params: {
+        filename
+      }
+    }
+    return axios.get(`${this.baseUrl}/api/GetUploadUrl`, config)
+      .then(response => response.data.url)
+  }
+
+  _uploadBlob(sasUrl, file) {
+    const config = {
+      headers: {
+        'X-ZUMO-AUTH': this.authToken,
+        'Content-Type': 'application/octet-stream',
+        'x-ms-version': '2017-04-17',
+        'x-ms-blob-type': 'BlockBlob',
+        'x-ms-blob-content-type': file.type
+      },
+      onUploadProgress(e) {
+        if (e.lengthComputable) {
+          console.log(e.loaded / e.total)
+        }
+      }
+    }
+    return axios.put(sasUrl, file, config)
+      .then(() => sasUrl)
   }
 }
+
+
 
 export default Api
